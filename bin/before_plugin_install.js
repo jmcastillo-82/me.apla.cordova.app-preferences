1ea9d6e63a751519ff00c122d14ba052265cac20
Juan Manuel Castillo Andrades <juanmanuel@flashexperience.net>
2020-09-28 13:21:52 +0200

    Fixed cordova9

--- bin/before_plugin_install.js
+++ bin/before_plugin_install.js
@@ -1,12 +1,10 @@
 'use strict';
 
-module.exports = function (context) {
-  var req = context.requireCordovaModule,
+module.exports = function(context) {
+    var path = require('path'),
+        pathParse = require('./lib/path-parse');
 
-      path = req ('path'),
-      pathParse = require ('./lib/path-parse');
+    path.parse = path.parse || pathParse;
 
-  path.parse = path.parse || pathParse;
-
-  return true;
+    return true;
 };
